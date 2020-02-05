b843b90741e1b24deb3c332e44fbcaa593273614
Anthony Nguyen <nvhoanganh1909@gmail.com>
2020-02-05 01:17:15 +0000

    service worker skip waiting

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  globDirectory: "dist\\apps\\tennis-score",
+  globDirectory: "dist/apps/tennis-score",
   maximumFileSizeToCacheInBytes: 5000000,
   globPatterns: ["**/*.{png,xml,ico,svg,json,html,js}"],
-  swDest: "dist/apps/tennis-score/sw.js",
-  swSrc: "apps/tennis-score/src/sw.js"
+  swDest: "dist/apps/tennis-score/sw.js"
+  //swSrc: "apps/tennis-score/src/sw.js"
 };
