f73e7621191c3d063524edafb3b1f97612aabe23
anthony <nvhoanganh1909@gmail.com>
2020-02-09 13:45:12 +1100

    fixed the bug

--- apps/tennis-score/src/app/hooks/useServiceWorker.ts
+++ apps/tennis-score/src/app/hooks/useServiceWorker.ts
@@ -26,7 +26,7 @@ const useServiceWorker = () => {
             }
           });
         });
-      });i
+      });
 
       let refreshing;
       navigator.serviceWorker.addEventListener("controllerchange", () => {
