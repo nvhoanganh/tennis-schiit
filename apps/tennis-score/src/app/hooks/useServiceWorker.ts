1374051ab7e76ca8c0aa0406dae7a0fa4a944f8d
anthony <nvhoanganh1909@gmail.com>
2020-02-09 14:14:04 +1100

    show reload

--- apps/tennis-score/src/app/hooks/useServiceWorker.ts
+++ apps/tennis-score/src/app/hooks/useServiceWorker.ts
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
-import { useToast } from "@chakra-ui/core";
+import { useEffect, useState } from "react";
+import { useToast, useDisclosure } from "@chakra-ui/core";
 const useServiceWorker = () => {
   const toast = useToast();
+  const { onClose } = useDisclosure();
+  const [showInstalling, setShowInstalling] = useState(false);
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.register("/sw.js").then(reg => {
@@ -10,12 +12,18 @@ const useServiceWorker = () => {
           let newWorker;
           newWorker = reg.installing;
           newWorker.addEventListener("statechange", () => {
-            toast({
-              title: "Installing new version...",
-              status: "success",
-              duration: null,
-              isClosable: false
-            });
+            if (!showInstalling) {
+              // show only once
+              toast({
+                title: "Installing new version...",
+                status: "success",
+                onClose: onClose,
+                duration: 3000,
+                isClosable: true
+              });
+              setShowInstalling(true);
+            }
+
             switch (newWorker.state) {
               case "installed":
                 console.log("new service worker installled");
@@ -31,6 +39,7 @@ const useServiceWorker = () => {
       let refreshing;
       navigator.serviceWorker.addEventListener("controllerchange", () => {
         if (refreshing) return;
+        onClose();
         toast({
           title: "New version installed. Reload to see updates",
           status: "success",
