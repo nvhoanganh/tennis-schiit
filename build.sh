c3c15c921749c70a77341e6111257ed04fe2f185
anthony <nvhoanganh1909@gmail.com>
2020-02-14 02:19:16 +1100

    update build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+# clean the dist folder
 rm -rf dist/apps/tennis-score/
 
 # get version
@@ -10,14 +11,13 @@ node ./node_modules/envsub/bin/envsub.js --env BuildId=$DATEV.$VERSION apps/tenn
 echo Start building the app
 npm run build-prod
 
-# replace the file
+# update the ServiceWorker shell
 echo Replace the index.html file
 value=$(<dist/apps/tennis-score/index.html)
 echo "$value"
 node ./node_modules/envsub/bin/envsub.js --env Template="$value" sw-shell-real.template.js functions/sw-shell-real.js
-# rm -rf dist/apps/tennis-score/index.html
 
-# inject service worker (after index.html is removed)
+# inject service worker
 echo Injecting the service worker
 workbox injectManifest workbox-config.js
 
