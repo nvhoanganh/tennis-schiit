ba81e9e4d571bb00fec1e4defa9d369b4b12c9a9
anthony <nvhoanganh1909@gmail.com>
2020-02-21 00:35:10 +1100

    update user profile on change

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,30 @@ exports.usersTrigger = functions.firestore
   .onUpdate((change, context) => {
     //users/{userId}
     const { userId } = context.params;
-    const { avatarUrl, groups } = change.after.data();
-    console.log(avatarUrl);
+    const after = change.after.data();
+    const before = change.before.data();
+
+    if (before.avatarUrl !== after.avatarUrl) {
+      console.log("avatar changed, updating groups");
+      return Promise.all(
+        Object.keys(after.groups).map(groupId => {
+          const groupRef = db.collection("groups").doc(groupId);
+          return groupRef.get().then(d =>
+            d.exists
+              ? groupRef.update({
+                  players: d.data().players.map(x =>
+                    x.userId === userId
+                      ? {
+                          ...x,
+                          avatarUrl: after.avatarUrl || ""
+                        }
+                      : x
+                  )
+                })
+              : Promise.resolve()
+          );
+        })
+      );
+    }
     return Promise.resolve();
   });
