a11e039c649bbbc0f5dd411deb4582e37ddd4356
anthony <nvhoanganh1909@gmail.com>
2020-03-02 12:26:39 +1100

    add tensorflowjs

---
+++ machine-learning/index.js
@@ -0,0 +1,22 @@
+// https://github.com/nvhoanganh/tennis-schiit/blob/master/machine-learning/model/model.json
+// JavaScript
+
+const tf = require("@tensorflow/tfjs");
+
+async function load() {
+  const model = await tf.loadModel(
+    "https://raw.githubusercontent.com/nvhoanganh/tennis-schiit/master/machine-learning/model/model.json"
+  );
+  // The first argument is the data, and the second is the shape.
+  const inputData = tf.tensor2d([[0.95, 0.3, 0]], [1, 3]);
+
+  // Get the highest confidence prediction from our model
+  const result = model.predict(inputData);
+  // const winner = irisClasses[result.argMax().dataSync()[0]];
+  // Display the winner
+  console.log(result);
+}
+
+// JavaScript
+load();
+// Get measurements for a new flower to generate a prediction
