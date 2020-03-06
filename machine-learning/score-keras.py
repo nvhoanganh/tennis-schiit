2d3779a7537b84e934e957562b8b9951a0b0de4f
Patrick Zhao <PatrickZhao@ssw.com.au>
2020-03-06 17:45:48 +1100

    tidy up code and implement the keras scoring script

---
+++ machine-learning/score-keras.py
@@ -0,0 +1,44 @@
+import json
+import numpy as np
+import os
+import keras
+
+#from azureml.assets.persistence.persistence import get_model_path
+from azureml.core.model import Model
+import logging
+
+
+def init():
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info("we are in init")
+    print(Model.get_model_path(model_name='ml-ws-keras-weight'))
+    logging.info("we are loading model")
+
+    logging.info("model loaded")
+
+def run(raw_data):
+    try:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.info("we are in scoring")
+        print(Model.get_model_path(model_name='ml-ws-keras-weight'))
+        logging.info("we are loading model")
+        model_root = Model.get_model_path(model_name='ml-ws-keras-weight')
+
+        model = keras.models.Sequential()
+        model.add(keras.layers.Dense(4, input_dim=3, activation='relu'))
+        model.add(keras.layers.Dense(4, activation='relu'))
+        model.add(keras.layers.Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer='adam',  metrics=['accuracy'])
+        model.load_weights(model_root)
+
+        logging.info("model loaded")
+        logging.info(raw_data)
+        data = np.array(json.loads(raw_data)['data'])
+        logging.info(data)
+        # make prediction
+        logging.info('ready to predict')
+        result = model.predict_classes(data)
+        logging.info(result)
+        return result.tolist()
+    except Exception as e:
+        return json.dumps({"error": str(e)})
--- machine-learning/train-keras.py
+++ machine-learning/train-keras.py
@@ -4,6 +4,16 @@ import keras
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
+
+def create_model():
+      model = keras.models.Sequential()
+      model.add(keras.layers.Dense(4, input_dim=3, activation='relu'))
+      model.add(keras.layers.Dense(4, activation='relu'))
+      model.add(keras.layers.Dense(1, activation='sigmoid'))
+      model.compile(loss='binary_crossentropy', optimizer='adam',  metrics=['accuracy'])
+      return model;
+
+
 df = pd.read_csv('tennis-result-2.csv')
 
 properties = list(df.columns.values)
@@ -21,28 +31,36 @@ y = y.values
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
 
-model = keras.models.Sequential()
-model.add(keras.layers.Dense(4, input_dim=3, activation='relu'))
-model.add(keras.layers.Dense(4, activation='relu'))
-model.add(keras.layers.Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam',  metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=50, batch_size=1)
-test_loss, test_acc = model.evaluate(X_test, y_test)
+training_model = create_model();
+training_model.fit(X_train, y_train, epochs=50, batch_size=1)
+test_loss, test_acc = training_model.evaluate(X_test, y_test)
 print('Test accuracy:', test_acc)
 
 
 
-x_predict = np.array([[0.05, 0.1, 0 ], [0.55, 0.7, 0]])
+x_predict = np.array([[0.05, 0.1, 0 ], [0.7, 0.55, 0]])
 
-result = model.predict_classes(x_predict)
+##result = training_model.predict_classes(x_predict)
 
 
-print(result)
+
 
 # serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
+model_json = training_model.to_json()
+with open("model/model.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
-model.save("model.h5")
+training_model.save("model/model.h5")
+
+
+training_model.save_weights("model/model_weight.h5")
+
+
+
+
+## load model and score
+loaded_model = create_model();
+loaded_model.load_weights("model/model_weight.h5")
+result = loaded_model.predict_classes(x_predict)
+print(result)
